f528a8adf1f176a2f2eb10787856da0cee7bf785
Sebastian Lague <sebastlague@gmail.com>
2020-12-04 16:08:29 +0100

    Set target frame rate

---
+++ Assets/Scripts/Graphics/TargetFrameRate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour {
+	void Awake () {
+		Application.targetFrameRate = 60;
+	}
+}
