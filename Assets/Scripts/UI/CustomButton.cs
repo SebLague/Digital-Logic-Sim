278af01d5432d0a26ad33f31d0f7015cd4dca244
Riley Worthen <rileyworthen.123@gmail.com>
2021-03-23 23:03:06 -0700

    remove unused code

--- Assets/Scripts/UI/CustomButton.cs
+++ Assets/Scripts/UI/CustomButton.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CustomButton : Button, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler {
+public class CustomButton : Button, IPointerDownHandler {
 	public event System.Action onPointerDown;
-	public event System.Action onPointerEnter;
-	public event System.Action onPointerExit;
 
 	public override void OnPointerDown (PointerEventData eventData) {
 		base.OnPointerDown (eventData);
 		onPointerDown?.Invoke ();
-	}
-
-	public override void OnPointerEnter (PointerEventData eventData) {
-		base.OnPointerEnter (eventData);
-		onPointerEnter?.Invoke ();
-	}
-
-	public override void OnPointerExit (PointerEventData eventData) {
-		base.OnPointerExit (eventData);
-		onPointerExit?.Invoke ();
 	}
 }
